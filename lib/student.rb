b956dc3184c77eb8816b005bc773a98d0ffc6152
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-24 15:02:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,11 +53,11 @@ def self.create(name, grade)
 end
 
 def self.new_from_db(row)
-    
+
     id = row[0]
     name = row[1]
     student.grade = row[2]
-    student
+    student.new(name, grade, id)
 end
 
 def self.find_by_name
